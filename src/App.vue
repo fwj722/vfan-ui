683530ef4ae68b2afe8a84740168383c16825368
静 <1508488207@qq.com>
2020-04-12 18:43:08 +0800

    更新scss预编译

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-button type="primary" size="medium">122</v-button>
+    <v-button size="small">122</v-button>
+    <v-button plain size="mini">122</v-button>
+    <v-button type="danger">122</v-button>
+    <v-button type="text" size="small" disabled>122</v-button>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import VfanUI from "@/components/";
+import "./theme/index.scss";
 Vue.config.productionTip = false;
-
+Vue.use(VfanUI);
 new Vue({
   router,
   render: h => h(App)
