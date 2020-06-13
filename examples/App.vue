dfe0b7fae889ebeac0a24d24151ff2102d15ed32
静 <1508488207@qq.com>
2020-06-13 18:10:24 +0800

    添加引导器

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-button type="default">默认按钮</v-button>
+    <v-button id="btn1" type="default">默认按钮</v-button>
     <v-button type="primary">主要按钮</v-button>
     <v-button type="success">成功按钮</v-button>
     <v-button type="warning">警告按钮</v-button>
@@ -25,13 +25,12 @@
       </div>
       <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
     </v-card>
-    <br/>
+    <br />
     <v-card shadow="always">总是显示</v-card>
-    <br/>
+    <br />
     <v-card shadow="hover">鼠标悬浮时显示</v-card>
-    <br/>
+    <br />
     <v-card shadow="never">从不显示</v-card>
-  
   </div>
 </template>
 <script>
@@ -41,7 +40,7 @@ export default {
       currentDate: new Date()
     };
   },
-  methods:{
+  methods: {
   }
 };
 </script>
--- packages/index.js
+++ packages/index.js
@@ -2,11 +2,14 @@
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 // 统一导出
 // 导入颜色选择器组件
+// import Vue from "vue"
 import VButton from "./button";
 import VCard from "./card";
 import "./theme/index.scss";
+import "./guide/style/guide.scss";
+import "./utils/guide";
 
-import vueDrag from './utils/vue-drag'
+import vueDrag from "./utils/vue-drag";
 // import Dialog from './dialog'
 // import Input from './input'
 // import Checkbox from './checkbox'
@@ -19,21 +22,20 @@ import vueDrag from './utils/vue-drag'
 // import './fonts/font.scss'
 
 // 存储组件列表
-const components = [VButton,VCard];
+const components = [VButton, VCard];
 const install = function(Vue) {
   // 全局注册所有的组件
-  components.forEach(item => {
+  components.forEach((item) => {
     Vue.component(item.name, item);
   });
   // 指令
   Vue.use(vueDrag);
 };
-
 // 判断是否是直接引入文件,如果是，就不用调用 Vue.use()
 if (typeof window !== "undefined" && window.Vue) {
   install(window.Vue);
 }
 
 export default {
-  install
+  install,
 };
